1e12e158286f29f3cc8ff5e590f7ef924d5942de
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-02 18:48:04 +0200

    wip

---
+++ app/Console/Commands/WalletBalanceCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class WalletBalanceCommand extends Command
+{
+    protected $signature = 'rune:wallet-balance';
+
+    protected $description = 'Retrieve the wallet balance for a Rune.';
+
+    public function handle()
+    {
+        //
+    }
+}
---
+++ app/Jobs/RetrieveWalletBalance.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class RetrieveWalletBalance implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public string $apiUrl;
+
+    public function __construct(
+        public Rune $rune,
+        public string $address,
+    ) {
+        $this->apiUrl = 'https://api-mainnet.magiceden.dev/v2/ord/btc/runes/wallet/balances/'.$this->address.'/'.$this->rune->tickerWithoutSpacers
+    }
+
+    public function handle(): void
+    {
+        $response = Http::withToken(
+            config('services.magiceden.api_key')
+        )
+            ->acceptJson()
+            ->get($this->apiUrl);
+
+        if ($response->successful()) {
+            //
+        }
+    }
+}
