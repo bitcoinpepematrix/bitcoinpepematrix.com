374265d3ce870041f24e861bb476c6cf4207774e
Mark van den Broek <mvdnbrk@protonmail.com>
2024-09-16 01:43:14 +0200

    Update WalletBalanceCommand.php

--- app/Console/Commands/WalletBalanceCommand.php
+++ app/Console/Commands/WalletBalanceCommand.php
@@ -8,7 +8,7 @@ class WalletBalanceCommand extends Command
 {
     protected $signature = 'rune:wallet-balance';
 
-    protected $description = 'Retrieve the wallet balance for a Rune.';
+    protected $description = 'Retrieve the wallet balance for a Rune';
 
     public function handle()
     {
