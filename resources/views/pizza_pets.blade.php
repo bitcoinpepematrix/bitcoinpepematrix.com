4a13127a87dc8ffc4f2b4124551b62bf1f12468d
Mark van den Broek <mvdnbrk@protonmail.com>
2024-10-30 15:03:54 +0100

    Update Pizza Pets page

--- resources/views/pizza_pets.blade.php
+++ resources/views/pizza_pets.blade.php
@@ -7,10 +7,10 @@
         <x-matrix-background/>
         <div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40">
             <div class="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl">
-                <h1 class="mt-2 px-4 text-2xl font-bold tracking-wider text-white sm:text-4xl">
+                <h1 class="mt-2 px-4 text-2xl font-bold tracking-wider text-slate-200 sm:text-4xl">
                     HAVE AN EGGCELENT DAY!
                 </h1>
-                <div class="mt-4 text-xl text-gray-100 bg-slate-950/75 p-4 rounded-xl">
+                <div class="mt-4 text-xl text-slate-200 bg-slate-950/75 p-4 rounded-xl">
                     <p>
                         We are thrilled to announce a collaboration with
                     </p>
@@ -33,7 +33,7 @@
                         >Pizza Pets</a> airdrop.
                     </p>
                     <p class="mt-6">
-                        More details coming soon...
+                        More details will be announced<br>in our Discord and Telegram.
                     </p>
                 </div>
             </div>
@@ -58,19 +58,27 @@
         </div>
     </div>
     <div class="mx-auto max-w-7xl px-6 pb-4 pt-4 lg:px-8">
-        <p class="text-white mt-6 p-4">
-            Be the first to know every detail about our collaboration. Join our
+        <div class="flex items-center">
+            <span class="text-slate-100 pr-4">JOIN OUR TELEGRAM:</span>
             <a
-                class="underline underline-offset-2 hover:underline-offset-4"
                 href="{{ $socials->telegram_url }}"
+                class="text-slate-100 hover:text-slate-400"
                 target="_blank"
-            >Telegram</a>
-            or
+                rel="noopener"
+            >
+                <span class="sr-only">Telegram</span>
+                <x-icon-telegram class="w-8 h-8"/>
+            </a>
+            <span class="text-slate-100 pl-12">JOIN OUR DISCORD:</span>
             <a
-                class="underline underline-offset-2 hover:underline-offset-4"
                 href="{{ $socials->discord_url }}"
+                class="pl-6 text-slate-100 hover:text-slate-400"
                 target="_blank"
-            >Discord</a> !
-        </p>
+                rel="noopener"
+            >
+                <span class="sr-only">Discord</span>
+                <x-icon-discord class="w-8 h-8"/>
+            </a>
+        </div>
     </div>
 </x-layout.main>
