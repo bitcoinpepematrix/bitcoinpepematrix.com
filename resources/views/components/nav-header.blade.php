95a92a6169eff04a56d250e2fefb24f49c028420
Mark van den Broek <mvdnbrk@protonmail.com>
2024-10-30 14:09:04 +0100

    Update nav header

--- resources/views/components/nav-header.blade.php
+++ resources/views/components/nav-header.blade.php
@@ -1,13 +1,21 @@
 <nav class="bg-green-950 text-white border-b border-b-green-800">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="relative flex h-12 items-center justify-between">
-            <a
-                href="{{ route('homepage') }}"
-                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-green-900 hover:text-white"
-            >
-                HOME
-            </a>
-            {{ $rune->ticker }}
+            <div class="flex space-x-4">
+                <a
+                    href="{{ route('homepage') }}"
+                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-green-900 hover:text-white"
+                >
+                    HOME
+                </a>
+                <a
+                    href="{{ route('memes') }}"
+                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-green-900 hover:text-white"
+                >
+                    MEMES
+                </a>
+            </div>
+            <span class="text-green-600">{{ $rune->ticker }}</span>
         </div>
     </div>
 </nav>
